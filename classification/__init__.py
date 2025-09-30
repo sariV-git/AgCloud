795b059fb173b3dee6a155cb9a3566668e61de85
T-H-10 <t0534198344@gmail.com>
2025-09-30 12:43:18 +0300

    remove some comments. #108

--- classification/__init__.py
+++ classification/__init__.py
@@ -1,5 +1,3 @@
-# classification/__init__.py
-
 """
 Top-level package for the project.
 Avoid importing heavy submodules at package import time to keep imports safe for tests.
@@ -7,25 +5,3 @@ Avoid importing heavy submodules at package import time to keep imports safe for
 
 __all__ = ["__version__"]
 __version__ = "0.0.0"
-
-# Optionally provide lazy accessors for subpackages (optional)
-# Do NOT import subpackages here to avoid import-time side effects.
-# If you need convenience imports, import them lazily in user code:
-#   from classification import pipeline  # this will import pipeline then
-
-
-# """Classification package for audio classification tasks."""
-
-# from . import backbones
-# from . import core
-# from . import head
-# from . import pipeline
-# from . import scripts
-
-# __all__ = [
-#     "backbones",
-#     "core",
-#     "head",
-#     "pipeline",
-#     "scripts"
-# ]

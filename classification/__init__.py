46859cc0f7947cf224ab53faf8947f01b413e4a3
T-H-10 <t0534198344@gmail.com>
2025-10-22 15:41:22 +0300

    remove microservice classification that not in correct place

--- classification/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Top-level package for the project.
-Avoid importing heavy submodules at package import time to keep imports safe for tests.
-"""
-
-__all__ = ["__version__"]
-__version__ = "0.0.0"
--- classification/pipeline/__init__.py
+++
@@ -1,4 +0,0 @@
-from .train_head import main as train_head_main
-from .infer import main as classify_main
-
-__all__ = ["train_head_main", "classify_main", "train_head"]

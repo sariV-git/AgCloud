6f67d94869cd9e16b796046b03a506ea0d24f756
Yehudit10 <yehudit50402@gmail.com>
2025-11-02 18:33:06 +0200

    adding alertmanger service

--- services/db_api_service/app/router.py
+++ services/db_api_service/app/router.py
@@ -2,27 +2,27 @@
 from fastapi import APIRouter, Depends
 from app.auth import require_auth
 from app.tables.files.router import router as files_router
-<<<<<<< HEAD
+
 from app.tables.devices.router import router as device_router
 from app.tables.incidents.router import router as incidents_router
-=======
+
 from app.tables.generic.router import build_generic_router
 from app.tables.task_thresholds.router import router as task_thresholds_router
->>>>>>> c2d14fa33488ec8d7ead3ec98c4d532094712632
 
 
-<<<<<<< HEAD
-api.include_router(files_router)
-api.include_router(device_router)
-api.include_router(incidents_router)
-=======
+
+
+# api.include_router(files_router)
+# api.include_router(device_router)
+# api.include_router(incidents_router)
+
 def build_router(contract_store) -> APIRouter:
     api = APIRouter(
         prefix="/api",
         tags=["api"],
         dependencies=[Depends(require_auth)],
     )
->>>>>>> c2d14fa33488ec8d7ead3ec98c4d532094712632
+
 
     api.include_router(files_router)
     api.include_router(task_thresholds_router)

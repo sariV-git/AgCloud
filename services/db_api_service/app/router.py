834b2f478933f78ffbe410f8ac1be7dbbe7e82ba
Yehudit10 <yehudit50402@gmail.com>
2025-11-03 11:19:47 +0200

    Refactor router.py to remove unused imports

--- services/db_api_service/app/router.py
+++ services/db_api_service/app/router.py
@@ -3,9 +3,6 @@ from fastapi import APIRouter, Depends
 from app.auth import require_auth
 from app.tables.files.router import router as files_router
 
-from app.tables.devices.router import router as device_router
-from app.tables.incidents.router import router as incidents_router
-
 from app.tables.generic.router import build_generic_router
 from app.tables.task_thresholds.router import router as task_thresholds_router
 
@@ -24,4 +21,5 @@ def build_router(contract_store) -> APIRouter:
     api.include_router(build_generic_router(contract_store))
 
     return api
+
 

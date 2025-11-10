1518f7a838714e143fbc28cfff7c50e555ac48d7
leahMalul <lm05567313@gmail.com>
2025-11-10 16:36:36 +0200

    yap

--- services/inference_http/model_registry.py
+++ services/inference_http/model_registry.py
@@ -1,20 +1,3 @@
-# from typing import Any, Dict
-# from adapters.fruit_defect_runner import FruitDefectRunner
-
-# class FruitRunner:
-#     def __init__(self):
-#         self.impl = FruitDefectRunner()
-
-#     def run(self, image_bytes: bytes, model_tag=None, extra=None) -> Dict[str, Any]:
-#         return self.impl.run(image_bytes, model_tag=model_tag, extra=extra)
-
-# def get_model_runner(team: str):
-#     t = (team or "").lower()
-#     if t == "fruit":
-#         return FruitRunner()
-#     raise ValueError(f"unknown TEAM {t}")
-
-
 from adapters.fruit_defect_runner import FruitDefectRunner
 from adapters.fruit_segmentation_runner import FruitSegmentationRunner
 

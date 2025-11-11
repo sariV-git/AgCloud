2b02960233450e211848a40ba6a9b29f1d371fec
Sara-git-218 <sarah74218@gmail.com>
2025-11-11 13:34:21 +0200

    Deduplicate: keep leaf-counting only under projects/, remove dags copy

--- airflow_bundle/leaf-pipeline/airflow/dags/leaf-counting/src/__init__.py
+++
@@ -1,5 +0,0 @@
-# decompyle3 version 3.9.3
-# Python bytecode version base 3.12.0 (3531)
-# Decompiled from: Python 3.12.3 (main, Aug 14 2025, 17:47:21) [GCC 13.3.0]
-# Embedded file name: /home/user/ml-workspace/projects/leaf-counting/src/__init__.py
-# Compiled at: 2025-10-20 13:47:51
